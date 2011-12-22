b223b7d430add1c91b591f443ef86ef856a14dda
Sean.Colombo <Sean.Colombo@242e9ea1-536c-fc6a-c546-2656ed4a42de>
2011-12-22 01:12:50 +0000

    Added the allimages call.

--- trunk/Mediawiki/LyricWiki.pm
+++ trunk/Mediawiki/LyricWiki.pm
@@ -8,7 +8,8 @@ use base qw(Mediawiki::API);
 use Data::Dumper;
 
 ##### CONFIGURATION #####
-my $LYRICWIKI_BASE_URL = "http://lyrics.wikia.com/api.php";
+#my $LYRICWIKI_BASE_URL = "http://lyrics.wikia.com/api.php";
+my $LYRICWIKI_BASE_URL = "http://lyrics.sean.wikia-dev.com/api.php";
 ##### CONFIGURATION #####
 
 =pod
@@ -43,12 +44,17 @@ sub new{
 
 #############################################################
 
+=item $api->getSong("Cake", "Dime")
+=item $api->getSong("Cake:Dime")
+
 ####
 # Calls the getSong function from LyricWiki.  Ideally, the
 # artist and song name should be passed in separately, but
 # if they are both in the same string (separated by a comma)
 # that can be passed in as the only param and it will work.
 ####
+
+=cut
 sub getSong{
 	my $self = shift;
 	my $artist = shift;
@@ -80,23 +86,18 @@ sub getSong{
 
 #############################################################
 
+=item $api->getArtist("Cake");
+
 ####
 # Returns info for an artist.
 ####
+
+=cut
 sub getArtist{
-my $self = shift;
+	my $self = shift;
 	my $artist = shift;
-	my $song = shift;
 
-	# If only one parameter was provided, split on the first colon.
-	if((!$song) || ($song eq "")){
-		if($artist =~ /^(.*?):(.*)$/){
-			$song = $2;
-			$artist = $1;
-		}
-	}
-
-	$self->print(1,"A Fetching content of \"$artist:$song\"");
+	$self->print(1,"A Fetching content of \"$artist\"");
  
 	my $queryParams = [
 		'forceget' => '1', # the LW API appears to expect GET data only (for GET requests).
@@ -114,24 +115,101 @@ my $self = shift;
 
 #############################################################
 
-# TODO: getHometown()
+=item $api->getHometown("Mac Miller");
+
+####
+# Returns the hometown for an artist.
+####
+
+=cut
 sub getHometown{
+	my $self = shift;
+	my $artist = shift;
+
+	$self->print(1,"A Fetching hometown for \"$artist\"");
+
+	my $queryParams = [
+		'forceget' => '1', # the LW API appears to expect GET data only (for GET requests).
+		'action' => 'lyrics', 
+		'func' => 'getHometown', 
+		'artist' => $artist,
+		'fmt' => 'xml',
+		];
+
+	my $results = $self->makeXMLrequest($queryParams);
+
+	return $results;
 } # end getHometown()
-# TODO: getHometown()
 
 #############################################################
 
-# TODO: getSOTD()
+=item $api->getSOTD();
+
+####
+# Returns info for the current Song of the Day.
+#####
+
+=cut
 sub getSOTD{
+	my $self = shift;
+
+	$self->print(1,"A Fetching Song of the Day");
+ 
+	my $queryParams = [
+		'forceget' => '1', # the LW API appears to expect GET data only (for GET requests).
+		'action' => 'lyrics', 
+		'func' => 'getSOTD', 
+		'fmt' => 'xml',
+		];
+
+	my $results = $self->makeXMLrequest($queryParams);
+
+	return $results;
 } # end getSOTD()
-# TODO: getSOTD()
 
 #############################################################
 
-# TODO: postSong()
+####
+# Creates a song page via the API.
+#
+# Params:
+# overwriteIfExists - nonzero means that this page data (the lyrics and onAlbums) will overwrite
+#                     the page if it already exists. 0 means that nothing will be changed if the
+#                     page already exists.
+# artist -
+# song - 
+# lyrics - 
+# language - optional string for the language (the language name should be in English).
+# onAlbums - an array-reference to an array which contains strings which are album titles.
+####
 sub postSong{
+	my $self = shift;
+	my $overwriteIfExists = shift;
+	my $artist = shift;
+	my $song = shift;
+	my $lyrics = shift;
+	my $language = shift;
+	my $onAlbums = shift;
+	$onAlbums = join("|", @{$onAlbums});
+
+	$self->print(1,"A Posting \"$artist:$song\"");
+
+	my $queryParams = [
+		'action' => 'lyrics',
+		'func' => 'postSong',
+		'overwriteIfExists' => $overwriteIfExists,
+		'artist' => $artist,
+		'song' => $song,
+		'lyrics' => $lyrics,
+		'onAlbums' => $onAlbums,
+		'language' => $language,
+		'fmt' => 'xml',
+		];
+
+	my $results = $self->makeXMLrequest($queryParams);
+
+	return $results;
 } # end postSong()
-# TODO: postSong()
 
 #############################################################
 
